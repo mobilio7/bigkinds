af9d8a1c0eb1e6ff6bb2378799a9235afa3b0d22
Sanglim Lee <82.limii@gmail.com>
2022-11-21 16:23:54 +0900

    es make index realtime

--- lsl_test/es_dev/make_index_realtime.py
+++ lsl_test/es_dev/make_index_realtime.py
@@ -1,128 +1,87 @@
-# import os
-# import time
-# from watchdog.observers import Observer
-# from watchdog.events import FileSystemEventHandler
-
-# class Target:
-#     watchDir = os.getcwd()
-#     # watchDir = os.chdir('../data')
-#     #watchDir에 감시하려는 디렉토리를 명시한다.
-
-#     def __init__(self):
-#         self.observer = Observer()   #observer객체를 만듦
-
-#     def run(self):
-#         event_handler = Handler()
-#         self.observer.schedule(event_handler, self.watchDir, 
-#                                                        recursive=True)
-#         self.observer.start()
-#         try:
-#             while True:
-#                 time.sleep(1)
-#         except:
-#             self.observer.stop()
-#             print("Error")
-#             self.observer.join()
-
-# class Handler(FileSystemEventHandler):
-# #FileSystemEventHandler 클래스를 상속받음.
-# #아래 핸들러들을 오버라이드 함
-
-#     #파일, 디렉터리가 move 되거나 rename 되면 실행
-#     def on_moved(self, event):
-#         print(event)
-
-#     def on_created(self, event): #파일, 디렉터리가 생성되면 실행
-#         print(event)
-
-#     def on_deleted(self, event): #파일, 디렉터리가 삭제되면 실행
-#         print(event)
-
-#     def on_modified(self, event): #파일, 디렉터리가 수정되면 실행
-#         print(event)
-
-# if __name__ == '__main__': #본 파일에서 실행될 때만 실행되도록 함
-#     w = Target()
-#     w.run()
-
-
-#--------------------------------------
-import time
 import os
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from newsml_to_json import make_json
+from elasticsearch import Elasticsearch
 
-try:
-    from watchdog.observers import Observer
-    from watchdog.events import FileSystemEventHandler
-except ModuleNotFoundError as e:
-    print (e)
-    os.system("pip install watchdog")
+# es = Elasticsearch('http://10.37.30.125:9199')
+es = Elasticsearch('http://localhost:9199')
 
-# ------------------------------------------------
-class Handler(FileSystemEventHandler):
-    def on_created(self, event): # 파일 생성시
-        print (f'event type : {event.event_type}\n'
+mapping = {
+    "settings" : {
+        "number_of_shards": 6,
+        "number_of_replicas": 1
+    }
+}
 
-               f'event src_path : {event.src_path}')
-        if event.is_directory:
-            print ("디렉토리 생성")
-        else: # not event.is_directory
-            """
-            Fname : 파일 이름
-            Extension : 파일 확장자 
-            """
-            Fname, Extension = os.path.splitext(os.path.basename(event.src_path))
-            '''
-             1. zip 파일
-             2. exe 파일
-             3. lnk 파일
-            '''
-            if Extension == '.zip':
-                print (".zip 압축 파일 입니다.")
-            elif Extension == '.exe':
-                print (".exe 실행 파일 입니다.")
-                os.remove(Fname + Extension)   # _파일 삭제 event 발생
-            elif Extension == '.lnk':
-                print (".lnk 링크 파일 입니다.")
+class Target:
 
-    def on_deleted(self, event):
-        print ("삭제 이벤트 발생")
-
-    def on_moved(self, event): # 파일 이동시
-        print (f'event type : {event.event_type}\n')
-
-class Watcher:
-    # 생성자
     def __init__(self, path):
-        print ("감시 중 ...")
-        self.event_handler = None      # Handler
-        self.observer = Observer()     # Observer 객체 생성
-        self.target_directory = path   # 감시대상 경로
-        self.currentDirectorySetting() # instance method 호출 func(1)
+        #watchDir에 감시하려는 디렉토리를 명시한다.
+        os.chdir(path)
+        self.watchDir = os.getcwd()
 
-    # func (1) 현재 작업 디렉토리
-    def currentDirectorySetting(self):
-        print ("====================================")
-        print ("현재 작업 디렉토리:  ", end=" ")
-        os.chdir(self.target_directory)
-        print ("{cwd}".format(cwd = os.getcwd()))
-        print ("====================================")
+        self.observer = Observer()   #observer객체를 만듦
 
-    # func (2)
     def run(self):
-        self.event_handler = Handler() # 이벤트 핸들러 객체 생성
-        self.observer.schedule(
-            self.event_handler,
-            self.target_directory,
-            recursive=False
-        )
-
-        self.observer.start() # 감시 시작
+        event_handler = Handler()
+        # schedule 실행
+        self.observer.schedule(event_handler, self.watchDir, 
+                                                       recursive=True)
+        self.observer.start()
         try:
-            while True: # 무한 루프
-                time.sleep(5) # 1초 마다 대상 디렉토리 감시
-        except KeyboardInterrupt as e: # 사용자에 의해 "ctrl + z" 발생시
-            print ("감시 중지...")
-            self.observer.stop() # 감시 중단
+            while True:
+                # 60초마다 실행
+                time.sleep(60)
+        except:
+            self.observer.stop()
+            print("Error")
+            self.observer.join()
 
-myWatcher = Watcher("../data")
-myWatcher.run()
+class Handler(FileSystemEventHandler):
+#FileSystemEventHandler 클래스를 상속받음.
+#아래 핸들러들을 오버라이드 함
+
+    #파일, 디렉터리가 move 되거나 rename 되면 실행
+    def on_moved(self, event):
+        print('move')
+        print(event)
+
+    def on_created(self, event): #파일, 디렉터리가 생성되면 실행
+        print('create')
+        if event.is_directory :
+            print('----------------- new directory -----------------')
+            print(event.src_path)
+        else :
+            print('----------------- index 추가 -----------------')
+            Fname, Extension = os.path.splitext(os.path.basename(event.src_path))
+            if Extension == '.xml':
+                #json 변환
+                json_dict = make_json(event.src_path)
+                print(json_dict)
+                date = "kpf_bigkindslab_v1.1_" + json_dict['NewsEnvelope']['DateAndTime'][:4]
+                print(date)
+                # index가 존재하는 지 확인
+                # index 없을 경우 index 생성 후 doc 추가
+                # if es.indices.exists(index=date)==False:
+                #     es.indices.create(index=date, body=mapping)
+                #     print('index create : '+date)
+
+                # result = es.index(index=date, doc_type="_doc", body=json_dict, id=Fname)
+                # print(result)
+
+
+    def on_deleted(self, event): #파일, 디렉터리가 삭제되면 실행
+        print('delete')
+        print(event)
+
+    def on_modified(self, event): #파일, 디렉터리가 수정되면 실행
+        print('modified')
+        print(event.src_path)
+
+if __name__ == '__main__': #본 파일에서 실행될 때만 실행되도록 함
+    # 실제 디렉토리 경로
+    # w = Target('/hadoop/newsml/data')
+    w = Target('../data')
+    w.run()
