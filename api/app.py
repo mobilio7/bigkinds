cb03ad6c8a5f5be4a4efcb8f77020f9a31c50869
Sanglim Lee <82.limii@gmail.com>
2022-11-24 15:25:25 +0900

    app modified

--- api/app.py
+++ api/app.py
@@ -3,13 +3,13 @@ from flask.templating import render_template
 
 from predict_module import summarize_test
 from baikal_tagger import nlp_tagger
-from keyword_module import keyword_ext
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
+# 기사 요약 sentences : String , result : String Array
 @app.route("/get_summary", methods=['POST'])
-def summary(): # 기사 요약
+def summary():
     test_context = request.form['sentences']
     rtn = summarize_test(test_context)
     response = dict()
@@ -17,8 +17,9 @@ def summary(): # 기사 요약
 
     return jsonify(response), 200
 
+# 형태소 분석 text : String , result : JSON
 @app.route("/get_tag", methods=['POST'])
-def tag(): # 형태소 분석
+def tag():
     test_context = request.form['text']
     res = nlp_tagger(test_context)
     response = dict()
@@ -26,6 +27,7 @@ def tag(): # 형태소 분석
 
     return jsonify(response), 200
 
+# 형태소 분석 text : String , result : String Array    
 @app.route("/get_keyword", methods=['POST'])
 def keyword(): # 키워드 추출
     test_context = request.form['text']
@@ -35,10 +37,9 @@ def keyword(): # 키워드 추출
 
     return jsonify(response), 200
 
-
 @app.route('/temp')
 def temp():
     return render_template('input_test.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5003, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
